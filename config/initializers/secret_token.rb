4998436e6b21cda380f04a56a035af5f3414d8d4
Ulric Toinon <ulric.toinon@gmail.com>
2014-07-12 20:37:27 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Toinon::Application.config.secret_key_base = 'ca4b2ee4cf86f9520ce2cc3f3fef1fb63133cc9f4d973e2f51e411e1bf5e1a3f125c7820fbf5021b0b6a2369e5814eadb49acbfbe5710535a1a498b667c97d4c'
