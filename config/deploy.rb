5c800a9c4aebd1c96cdb68f2ff21c8dc77d8ba94
Ulric Toinon <ulric.toinon@gmail.com>
2014-07-16 20:55:43 +0200

    modify deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -89,7 +89,7 @@ namespace :deploy do
   %w[start stop restart].each do |command|
     desc "#{command} unicorn server"
     task command, roles: :app, except: {no_release: true} do
-      run "/etc/init.d/unicorn_toinon #{command}"
+      sudo "sh /etc/init.d/unicorn_toinon #{command}"
     end
   end
 
